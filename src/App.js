eb357b0f10cee99ae1a99bccd3257afd7fa8ab06
otienoirene208-lab <otienoirene208@gmail.com>
2026-06-03 09:46:58 +0300

    About

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import Firstcomponent from './components/Firstcomponent';
 import Secondcomponent from './components/Secondcomponent';
 import Taskcomponent from './components/Taskcomponent';
@@ -13,6 +14,7 @@ function App() {
         <Firstcomponent/>
         <Secondcomponent/>
         <Taskcomponent/>
+        <About/>
       
       </header>
     </div>
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+const About =()=> {
+    return(
+      <div className="About">
+        <h3>What is  react </h3>
+        <p>React is the library  for web and native user interface</p>
+
+        <h3>What is App.js</h3>
+        <p>is the root react component  that organizes your user  structure  and contains most of your app's logic</p>
+
+        <h4>What is index.js</h4>
+        <p>is a file that start your app and tells react where to display it in the web page </p>
+      </div>  
+    )
+}
+export default About;
